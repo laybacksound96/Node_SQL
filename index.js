3f57ab59ac6a3223a659451c5d9745798bbf1983
412 <laybacksound96@gmail.com>
2023-04-12 20:12:08 +0900

    첫 쿼리

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const { faker } = require("@faker-js/faker");
-var mysql = require("mysql");
 require("dotenv").config();
-
-var connection = mysql.createConnection({
+const { faker } = require("@faker-js/faker");
+const mysql = require("mysql");
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -15,13 +14,13 @@ connection.connect(function (error) {
     return;
   }
   console.log("Connected to MySQL");
+});
 
-  const query = "SELECT 1+3 as ss";
-  connection.query(query, function (error, results, fields) {
-    if (error) {
-      console.error("쿼리 실행 실패: ", error);
-      return;
-    }
-    console.log("쿼리 실행 결과: ", results);
-  });
+const query = "SELECT COUNT(*) AS total FROM users";
+connection.query(query, (error, results, fields) => {
+  if (error) {
+    console.error("쿼리 실행 실패: ", error);
+    return;
+  }
+  console.log("쿼리 실행 결과: ", results[0].total);
 });
