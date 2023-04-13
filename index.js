5b3273f96a52ab5771385bab224e5cf221ddb384
412 <laybacksound96@gmail.com>
2023-04-13 10:13:58 +0900

    04-13

--- index.js
+++ index.js
@@ -15,12 +15,23 @@ connection.connect(function (error) {
   }
   console.log("Connected to MySQL");
 });
-
-const query = "SELECT COUNT(*) AS total FROM users";
-connection.query(query, (error, results, fields) => {
-  if (error) {
-    console.error("쿼리 실행 실패: ", error);
-    return;
+const person = {
+  email: faker.internet.email(),
+  created_at: faker.date.past(),
+};
+var data = [];
+for (var i = 0; i < 500; i++) {
+  data.push([faker.internet.email(), faker.date.past()]);
+}
+connection.query(
+  "INSERT INTO users(email, created_at) VALUES ?",
+  [data],
+  (error, results, fields) => {
+    if (error) {
+      console.error("쿼리 실행 실패: ", error);
+      return;
+    }
+    console.log("쿼리 실행 결과: ", results);
   }
-  console.log("쿼리 실행 결과: ", results[0].total);
-});
+);
+connection.end();
